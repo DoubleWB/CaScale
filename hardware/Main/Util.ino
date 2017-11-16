d5e034a179c680ba63a6b2644340a4e83ab247bd
Will Becker <dustfingerss@gmail.com>
2017-11-15 19:02:03 -0500

    Changes for basic util class without tests

--- hardware/Main/Util.ino
+++ hardware/Main/Util.ino
@@ -40,13 +40,23 @@ class Stack {
 template <class T>
 class dNode {
   public:
-    dNode(T d) : data(d), next(NULL) {};
+    dNode(T d) : data(d), next(NULL), prev(NULL) {};
+    dNode(dNode<T>* p, dNode<T>* n) : data(NULL), next(n), prev(p) {};
     dNode(T d, dNode<T>* p, dNode<T>* n) : data(d), prev(p), next(n) {};
     dNode<T>* getNext() {
       return next;
     }
+    dNode<T>* getPrev() {
+      return prev;
+    }
     T getData() {
       return data;
+    }
+    void newPrev(dNode<T>* p) {
+      prev = p;
+    }
+    void newNext(dNode<T>* n) {
+      next = n;
     }
   private:
     T data;
@@ -57,7 +67,96 @@ class dNode {
 template <class T>
 class LinkedList {
     public:
-      LinkedList() : s() {};
+      LinkedList() : head(NULL) {
+        head->newPrev(head);
+        head->newNext(head);
+        len = 0;
+      }
+      void add(T element) {
+        dNode<T> newNode = new dNode<T>(element);
+        dNode<T>* end = head->getPrev();
+        newNode.newPrev(end);
+        end->newNext(&newNode);
+        newNode.newNext(head);
+        head->newPrev(&newNode);
+        len += 1;
+      }
+      void add(T element, int index) {
+        dNode<T>* temp = head;
+        while (index >= 0) {
+          temp = temp->getNext();
+        }
+        dNode<T> newNode = new dNode<T>(element);
+        dNode<T>* next = temp->getNext();
+        newNode.newPrev(temp);
+        temp->newNext(&newNode);
+        newNode.newNext(next);
+        next->newPrev(&newNode);
+        len += 1;
+      }
+      void remove(int index) {
+        dNode<T>* temp = head;
+        while (index >= 0) {
+          temp = temp->getNext();
+        }
+        dNode<T>* oldPrev = temp->getPrev();
+        dNode<T>* oldNext = temp->getNext();
+        oldPrev->newNext(oldNext);
+        oldNext->newPrev(oldPrev);
+        delete temp; // Clear pointer
+        temp = NULL;
+        len -= 1;
+      }
+      T get(int index) {
+        dNode<T>* temp = head;
+        while (index >= 0) {
+          temp = temp->getNext();
+        }
+        return temp->getData();
+      }
+      int length() {
+        return len;
+      }
     private:
-      Stack<T> s;
+      dNode<T>* head;
+      int len;
 };
+
+class CharList {
+  public:
+    CharList() : characters() {}
+    CharList(char* s, int length) {
+      int indexCount = 0;
+      while (length < 0) {
+        characters.add(s[indexCount]);
+        indexCount += 1;
+        length -= 1;
+      }
+    }
+    char* toChar() {
+      int tempLen = this->length();
+      char* out = new char[tempLen + 1];
+      int index = 0;
+      while (index < this->length()) {
+        out[index] = characters.get(index);
+      }
+      out[tempLen] = '\0'; //Make null terminated string appropriately
+    }
+    CharList operator+ (const CharList& c) {
+      int otherLen = c.length();
+      int index = 0;
+      while (index < otherLen) {
+        characters.add(c.get(index));
+      }
+    }
+    char get(int index) {
+      characters.get(index);
+    }
+    int length() {
+      return characters.length();
+    }
+  private:
+    LinkedList<char> characters;
+};
+
+
